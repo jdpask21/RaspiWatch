02b7bab7ea4979a7c0c92c982e192bcf8301a0d9
ikeda.takuma <ikeda0apple@gmail.com>
2024-09-16 17:36:50 +0900

    Added source and requirements

---
+++ watch_app/src/get_now_time.py
@@ -0,0 +1,12 @@
+import datetime
+
+WEEK_DAY = ["(MON)", "(TUE)", "(WED)", "(THU)", "(FRI)", "(SAT)", "(SUN)"]
+
+def get_now_time():
+    dt = datetime.datetime.now()
+    str_month_day = "{}/{}".format(dt.month, dt.day)
+    str_weekday = WEEK_DAY[dt.weekday()]
+    return dt.hour, dt.minute, str_month_day, str_weekday
+
+if __name__=="__main__":
+    print(get_now_time())
